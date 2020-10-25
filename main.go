dc58a934fddd9a96a3a4661e0833da78ecf669e1
TUANKUN <Tuankun@TuanKunnoMacBook-Pro.local>
2020-10-25 16:33:12 +0900

    ファイルをアップロードする処理の作成

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("template/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		mf, fh, err := r.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "pics", fh.Filename)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+	}
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
