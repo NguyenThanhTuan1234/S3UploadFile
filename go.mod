5431b93ab8f0845ededc85a863dda362903e8096
TUANKUN <Tuankun@TuanKunnoMacBook-Pro.local>
2020-10-27 00:02:30 +0900

    S3にファイルアップロードする処理を作成する

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/NguyenThanhTuan1234/S3UploadFile
+
+go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.35.14
+	github.com/aws/aws-sdk-go-v2/config v0.2.0
+	github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs v0.27.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v0.27.0
+)
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"html/template"
 	"io"
 	"net/http"
 	"os"
-	"path/filepath"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 var tpl *template.Template
+var svcS3 *s3.Client
 
 func init() {
 	tpl = template.Must(template.ParseGlob("template/*"))
@@ -28,18 +34,31 @@ func index(w http.ResponseWriter, r *http.Request) {
 			fmt.Println(err)
 		}
 		defer mf.Close()
-		wd, err := os.Getwd()
-		if err != nil {
-			fmt.Println(err)
-		}
-		path := filepath.Join(wd, "public", "pics", fh.Filename)
-		nf, err := os.Create(path)
+		nf, err := os.Create(fh.Filename)
 		if err != nil {
 			fmt.Println(err)
 		}
 		defer nf.Close()
 		mf.Seek(0, 0)
 		io.Copy(nf, mf)
+		fileInfo, _ := nf.Stat()
+		size := fileInfo.Size()
+		buffer := make([]byte, size)
+		fileBytes := bytes.NewReader(buffer)
+		cfg, err := config.LoadDefaultConfig()
+		if err != nil {
+			panic(err)
+		}
+		svcS3 = s3.NewFromConfig(cfg)
+		putObjectInput := s3.PutObjectInput{
+			Bucket: aws.String("s3testtuan1"),
+			Key:    aws.String(nf.Name()),
+			Body:   fileBytes,
+		}
+		_, err = svcS3.PutObject(context.Background(), &putObjectInput)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 	tpl.ExecuteTemplate(w, "index.html", nil)
 }
